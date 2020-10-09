79d040a5969122f45e0cbada6e9c36cf3b4c8743
Ratanpriyapandey <46558206+Ratanpriyapandey@users.noreply.github.com>
2020-10-09 16:35:17 +0200

    Delete todo.ts

--- model/todo.ts
+++
@@ -1,9 +0,0 @@
-export class Todo {
-    constructor(
-        public id: number,
-        public taskName: string,
-        public description: string,
-        public dateTime: string,
-        public priority: string     
-    ) { }
-}
